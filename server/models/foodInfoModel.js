bc7b9317c6acbdfe40a65d080d07626b458fd5dc
Kevin <kev776522.mg08@nycu.edu.tw>
2023-09-06 21:05:54 +0800

    feat backend: modify foodinfo api add image url in the result of sql search

--- server/models/foodInfoModel.js
+++ server/models/foodInfoModel.js
@@ -5,7 +5,7 @@ const getFoodInfoModel = async (page = 0) => {
   const conn = await pool.getConnection();
   try {
     const response = await conn.query(
-      'select * from food_info limit ?, ?', [page * parseInt(process.env.listPerPage), parseInt(process.env.listPerPage)]
+      'select food_id, food_name, carbonhydrate, protein, fat, calory, image_url from food_info, image_info where food_info.image_id=image_info.image_id limit ?, ?', [page * parseInt(process.env.listPerPage), parseInt(process.env.listPerPage)]
     );
     return response;
   } catch (err) {
@@ -20,7 +20,7 @@ const getFoodDetailModel = async (foodid = 1) => {
   const conn = await pool.getConnection();
   try {
     const response = await conn.query(
-      'select * from food_info where food_id = ?', [foodid]
+      'select food_id, food_name, carbonhydrate, protein, fat, calory, image_url from food_info, image_info where food_info.image_id=image_info.image_id and food_id = ?', [foodid]
     );
     return response;
   } catch (err) {
