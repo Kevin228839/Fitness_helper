2619a212426ecc52db7cd92f0ae697d6587a7858
Kevin <kev776522.mg08@nycu.edu.tw>
2023-11-27 00:10:57 +0800

    set main as https server

--- server/index.js
+++ server/index.js
@@ -5,17 +5,17 @@ const rateLimiter = require('./rateLimit');
 const foodInfoRouter = require('./routers/foodInfoRouter');
 const oauthRotuer = require('./routers/oauthRotuer');
 const userRouter = require('./routers/userRouter');
-const cors = require('cors');
+// const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// const path = require('path');
-// const https = require('https');
-// const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
-const corsOption = {
-  origin: 'http://localhost:3000',
-  credentials: true
-};
-app.use(cors(corsOption));
+// const corsOption = {
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// };
+// app.use(cors(corsOption));
 app.use(cookieParser());
 app.use(rateLimiter);
 app.use('/', foodInfoRouter);
@@ -23,12 +23,12 @@ app.use('/', oauthRotuer);
 app.use('/', userRouter);
 
 // for frontend url (after react build)
-// app.use(express.static(path.join(__dirname, '../frontend/build')));
-// app.use('/', function (_req, res, next) {
-//   res.sendFile(path.join(__dirname, '../frontend/build/index.html'), function (err) {
-//     next(err);
-//   });
-// });
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use('/', function (_req, res, next) {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'), function (err) {
+    next(err);
+  });
+});
 
 // error handler should put as the last middleware
 app.use((err, _req, res, _next) => {
@@ -37,11 +37,11 @@ app.use((err, _req, res, _next) => {
   res.status(statusCode).json({ message: err.message });
 });
 
-app.listen(port, console.log(`Server is listening at port:${port}`));
+// app.listen(port, console.log(`Server is listening at port:${port}`));
 
-// https.createServer({
-//   key: fs.readFileSync('../ssl/key.pem'),
-//   cert: fs.readFileSync('../ssl/certificate.pem')
-// }, app).listen(port, () => {
-//   console.log(`Server is listening at port:${port}`);
-// });
+https.createServer({
+  key: fs.readFileSync('../ssl/key.pem'),
+  cert: fs.readFileSync('../ssl/certificate.pem')
+}, app).listen(port, () => {
+  console.log(`Server is listening at port:${port}`);
+});
